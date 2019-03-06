72ad32c611fafff779ffd6acdaf56afc55c0d4ef
Sachin Mahajan <sachinm78@gmail.com>
2019-03-06 16:47:10 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,5 +3,7 @@ def run_guessing_game
   roll = 1+rand(6)
   user_input = gets.chomp.to_s 
     if user_input == roll
+      puts "You guessed the correct number!"
+    elsif user_input != roll
       puts ""
 end
